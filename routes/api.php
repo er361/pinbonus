6fba492e5d10a9c37ba7739dd1e96d05fe401a77
sf <sf7kmmr@gmail.com>
2020-11-03 14:57:27 +0600

    test log

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('logTest',function(){
+    \Illuminate\Support\Facades\Log::info('test');
+});
